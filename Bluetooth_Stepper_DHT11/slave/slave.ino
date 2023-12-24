1f5837465cf31b384430f193fb7476d623ff0b78
unknown <sontranviet21@gmail.com>
2023-12-24 11:51:25 +0700

    update

--- Bluetooth_Stepper_DHT11/slave/slave.ino
+++ Bluetooth_Stepper_DHT11/slave/slave.ino
@@ -5,7 +5,10 @@
 #include <SoftwareSerial.h>
 #include <Stepper.h>
 const int STEPS = 2048;
+
+// Cắm dây như hình: In1 = 8 = xanh lá , In2 = 9 = xanh biển, In3 = 10 = tím, In4 = 11 =xám
 Stepper myStepper = Stepper(STEPS, 8, 10, 9, 11);
+
 
 SoftwareSerial BTSerial(5, 6);
 
