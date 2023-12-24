4a31f1046171ce4c2ab7bfbf8e76719dab0b60ef
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2023-12-24 11:34:59 +0700

    addWifiServoLight

---
+++ Wifi_Servo_AnhSang/Wifi_Servo_AnhSang.ino
@@ -0,0 +1,128 @@
+#include <Scheduler.h>
+#include <Task.h>
+#include <LeanTask.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+// #include <DHT.h>
+#include <Servo.h>
+
+
+// Thiết lập thông số cho WiFi
+const char* ssid = "123456789";
+const char* password = "1234@56789";
+
+// Khởi tạo đối tượng máy chủ web
+ESP8266WebServer server(80);
+
+// Thiết lập cổng cho DHT và Servo
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define SERVO_PIN 4
+#define LIGHT 0
+
+// DHT dht(DHTPIN, DHTTYPE);
+Servo servo;
+
+int servo_angle = 90;
+class BlinkTask : public Task {
+ protected:
+  void setup() {
+  }
+
+  void loop() {
+    servo.write(servo_angle);
+    delay(1000);
+    servo.write(0);
+    delay(1000);
+  }
+
+} servoSpin;
+
+// void handleTemp() {
+//   // Đọc nhiệt độ từ cảm biến DHT
+//   float temperature = dht.readTemperature();
+//   Serial.println(temperature);
+//   if (isnan(temperature)) {
+//     server.send(500, "text/plain", "Lỗi khi đọc nhiệt độ từ cảm biến");
+//   } else {
+//     server.send(200, "text/plain", String(temperature));
+//   }
+// }
+
+// void handleHum() {
+//   // Đọc độ ẩm từ cảm biến DHT
+//   float humidity = dht.readHumidity();
+//   Serial.println(humidity);
+//   if (isnan(humidity)) {
+//     server.send(500, "text/plain", "Lỗi khi đọc độ ẩm từ cảm biến");
+//   } else {
+//     server.send(200, "text/plain", String(humidity));
+//   }
+// }
+
+void handleLight() {
+  // Đọc độ ẩm từ cảm biến DHT
+  int value = analogRead(LIGHT);
+  Serial.println(value);
+  if (isnan(value)) {
+    server.send(500, "text/plain", "Lỗi khi đọc độ ẩm từ cảm biến");
+  } else {
+    server.send(200, "text/plain", String(value));
+  }
+}
+
+void handleServo() {
+  // Xử lý vận hành Servo ở đây, dựa trên tham số được truyền (eg)
+  if (server.hasArg("angle")) {
+    servo_angle = server.arg("angle").toInt()*2;
+    // Serial.println(servo_angle);
+    // servo.write(servo_angle*2);
+    // delay(1000);
+    // servo.write(0);
+    // delay(1000);
+    server.send(200, "text/plain", "Servo đã được điều khiển");
+  } else {
+    server.send(400, "text/plain", "Thiếu tham số cho Servo");
+  }
+}
+
+
+void setup() {
+  // Khởi động Serial để theo dõi thông tin debug
+  Serial.begin(9600);
+
+  // Kết nối WiFi
+  WiFi.begin(ssid, password);
+  Serial.println("Kết nối đến WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Đã kết nối thành công!");
+  // Định nghĩa các endpoint và gán hàm xử lý tương ứng
+  // server.on("/dht/temp", handleTemp);
+  // server.on("/dht/hum", handleHum);
+  server.on("/light",HTTP_GET, handleLight);
+  server.on("/servo", HTTP_GET, handleServo);
+
+  // Khởi động máy chủ
+  server.enableCORS(true);
+  server.begin();
+  Serial.println("Server started");
+  Serial.print("IP Address of network: "); // will IP address on Serial Monitor
+  Serial.println(WiFi.localIP());
+  Serial.print("Copy and paste the following URL: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+  // Bắt đầu cảm biến DHT và Servo
+  // dht.begin();
+  servo.attach(SERVO_PIN);
+  Scheduler.start(&servoSpin);
+
+  Scheduler.begin();
+}
+
+void loop() {
+  server.handleClient(); // Xử lý các yêu cầu từ client
+}
